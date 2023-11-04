c263c58bc35915a08a132482656c367568101e64
premnadh <premnathbr123@gmail.com>
2023-11-04 22:29:50 +0530

    doing

--- practice.dart
+++ practice.dart
@@ -1,10 +1,7 @@
 void main() {
- var mySize = 174;
-print(mySize);
-int yourSize = 174;
-print(yourSize);
-double thisSize = 1.74;
-print(thisSize);
-dynamic oneSize = 'Onehundredandseventyfour' + ': ' + '174';
-print(oneSize);
+String text1 = 'this is a single line string';
+String text2 = '''this is a multiline
+	line string with a break''';
+print(text1);
+print(text2);
 }

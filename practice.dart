9255c021194f2d48d086f47aeaeb38646763cf53
premnadh <premnathbr123@gmail.com>
2023-11-04 22:29:07 +0530

    doing

--- practice.dart
+++ practice.dart
@@ -1,3 +1,10 @@
 void main() {
- 
+ var mySize = 174;
+print(mySize);
+int yourSize = 174;
+print(yourSize);
+double thisSize = 1.74;
+print(thisSize);
+dynamic oneSize = 'Onehundredandseventyfour' + ': ' + '174';
+print(oneSize);
 }

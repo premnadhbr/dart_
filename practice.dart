a84befa4bcda3347218dbef771ac83aebee7ed0e
premnadh <premnathbr123@gmail.com>
2023-11-06 18:36:08 +0530

    named constructors

--- practice.dart
+++ practice.dart
@@ -1,16 +1,43 @@
+
 void main() {
-  List<String> actorsName = [
-    'mohanlal',
-    'mamooty',
-    'pritviraj',
-    'raj',
-    'me',
-    'kajol',
-    
-  ];
-  int count = 0;
-  while (count < actorsName.length) {
-    print(actorsName[count]);
-    count++;
+  Student student = Student(name: 'premnadh', age: 23, marks: [10, 20, 30]);
+
+  Student student1 = Student.done(age: 12, marks: [12, 22, 33], name: 'Alli');
+
+  Student student2 =
+      Student.checking(age: 33, marks: [22, 33, 44, 55], name: 'Arya');
+  // student.studentDetails();
+}
+
+class Student {
+  String name;
+  num age;
+  List<num> marks;
+  Student({
+    required this.name,
+    required this.age,
+    required this.marks,
+  }) {
+    print("normall");
+  }
+
+  Student.checking({
+    required this.age,
+    required this.marks,
+    required this.name,
+  }) {
+    print('checking');
+  }
+
+  Student.done({
+    required this.age,
+    required this.marks,
+    required this.name,
+  }) {
+    print('Done');
+  }
+
+  void studentDetails() {
+    print("Student name :$name his age :$age his marks $marks");
   }
 }

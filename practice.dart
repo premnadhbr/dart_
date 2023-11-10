e0d012f1ffc8c3abb4561a89ed090c24e978ef35
premnadh <premnathbr123@gmail.com>
2023-11-10 21:54:41 +0530

    inheritance

--- practice.dart
+++ practice.dart
@@ -1,30 +1,25 @@
+// multiple inheritance in dart
 
 void main() {
-  Student student =
-      Student(className: "12TH", marks: [10, 20, 30], name: 'Premnadh');
+  C c = C();
 
-  student.printDetails();
 }
 
-class Student {
-  String name;
-  String className;
-  var marks;
-
-  Student({
-    required this.className,
-    required this.marks,
-    required this.name,
-  }) {
-    this.className = className;
-    this.marks = marks;
-    this.name = name;
+class A {
+  A() {
+    print("Hello...");
   }
+  
+}
 
-  printDetails() {
-    print("Student Details");
-    print("class Name :" + this.className);
-    print("marks :" + this.marks.toString());
-    print("Name :" + this.name);
+class B extends A{
+  B() {
+    print("Hey...");
   }
+  
+ 
+}
+
+class C {
+  B b = B();
 }

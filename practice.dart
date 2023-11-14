7bd57b38e247b1836fac32616d131d6368af4b37
premnadh <premnathbr123@gmail.com>
2023-11-14 12:11:31 +0530

    done

--- practice.dart
+++ practice.dart
@@ -1,8 +1,37 @@
 void main() {
- 
+  // fun();
+  print(funOne());
+  meen(12, 22);
+  sum(22, 22);
 }
 
+// no argument and no return type
+void fun() {
+  int a = 10;
+  int b = 10;
+  int sum = a + b;
+  print("Sum :$sum");
+}
 
- void calculateAmount(){
-  
- }
+//no argument and return type
+
+int funOne() {
+  int a = 100;
+  int b = 10;
+  int sum = a + b;
+  return sum;
+}
+
+//with argument and without return value
+
+void meen(int x, int y) {
+  int sum = x + y;
+  print(sum);
+}
+
+// function with argument and return type
+
+int sum(int a, int b) {
+  int sum = a + b;
+  return sum;
+}

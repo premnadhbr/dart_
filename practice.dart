c5463578489c66605b3ae7256aa92d7757ed273f
premnadh <premnathbr123@gmail.com>
2023-11-14 17:53:36 +0530

    annonymous func

--- practice.dart
+++ practice.dart
@@ -1,38 +1,6 @@
 void main() {
-  // fun();
-  print(funOne());
-  meen(12, 22);
-  int a = sum(1, 1);
-  print(a);
-}
-
-// no argument and no return type
-void fun() {
-  int a = 10;
-  int b = 10;
-  int sum = a + b;
-  print("Sum :$sum");
-}
-
-//no argument and return type
-
-int funOne() {
-  int a = 100;
-  int b = 10;
-  int sum = a + b;
-  return sum;
-}
-
-//with argument and without return value
-
-void meen(int x, int y) {
-  int sum = x + y;
-  print(sum);
-}
-
-// function with argument and return type
-
-int sum(int a, int b) {
-  int sum = a + b;
-  return sum;
+  List<int> me = [1, 2, 3, 4, 5, 6, 7, 8];
+  me.forEach((element) {
+    print(element);
+  });
 }

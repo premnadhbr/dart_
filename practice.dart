70b72b83d560de69c7245a084f34f8916e993d4d
premnadh <premnathbr123@gmail.com>
2023-11-14 09:48:40 +0530

    commit

--- practice.dart
+++ practice.dart
@@ -1,16 +1,8 @@
-enum me { constant_value1, constant_value2, constant_value3 }
-
 void main() {
-  const mee =me.constant_value1;
-  switch (mee) {
-    case me.constant_value1:
-      print("One");
-      break;
-    case me.constant_value2:
-      print("Two");
-      break;
-    case me.constant_value3:
-      print("Three");
-      break;
-  }
+ 
 }
+
+
+ void calculateAmount(){
+  
+ }

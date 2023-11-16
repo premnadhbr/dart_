84d0aef20e1a31f299626cd16017f2f76e9a51e1
premnadh <premnathbr123@gmail.com>
2023-11-16 18:41:44 +0530

    main

--- me.dart
+++
@@ -1,8 +0,0 @@
-import 'practice.dart';
-
-void main() {
-  Myname myname = Myname('premnadh', 22);
-
-  print(myname.result);
-  print(myname.updateval = 12);
-}
--- practice.dart
+++ practice.dart
@@ -1,18 +1,8 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+void main()
 
-class Myname {
-  String _name;
-  int _number;
-  Myname(
-    this._name,
-    this._number,
-  );
 
-  String get result {
-    return _name;
-  }
+{
 
-  set updateval(int newval) {
-    _number = newval;
-  }
+
+  
 }
